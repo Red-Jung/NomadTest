2ef7a4d53759e106245afc7f36368e3e8dcf4e43
Red-Jung <applered80@gmail.com>
2017-04-12 13:14:19 +0900

    start

---
+++ Nomad/src/main/java/Questing.java
@@ -0,0 +1,14 @@
+/**
+ * 테스트 코드를 보고, 아래 두 method 를 작성해주세요.
+ */
+class Questing {
+	static int quest1(double d)  {
+		// FIXME
+		return -1;
+	}
+
+	static boolean quest2(String s) {
+		// FIXME
+		return false;
+	}
+}
